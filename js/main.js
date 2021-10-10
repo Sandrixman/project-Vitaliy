b49f0419589c2405881f4d692556ddc2e48f4ae3
Aleksander <sandrix@ukr.net>
2021-10-10 21:45:56 +0300

    project completed

--- js/main.js
+++ js/main.js
@@ -3,9 +3,8 @@ $(function () {
     $('.graduate-slider').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
-        dots: true,
-        prevArrow: '<button class="products-slider__slider-btn products-slider__slider-btnprev"><img src="/images/left-arrow.svg"     alt=""></button> ',
-        nextArrow: `<button class="products-slider__slider-btn products-slider__slider-btnnext"><img src="/images/right-arrow.svg" alt="" ></button> `
+        prevArrow: '<button class="slider-btn slider-btnprev"><img src="./img/left-arrow.png" alt=""></button>',
+        nextArrow: `<button class="slider-btn slider-btnnext"><img src="./img/right-arrow.png" alt="" ></button> `
     });
 
 });
